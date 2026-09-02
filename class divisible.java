ac0f39a01c12953e15940c565d1f0fd7593a0ed8
unvithakl-hue <unvithakl@gmail.com>
2026-09-02 10:13:11 +0530

    Add files via upload

---
+++ class divisible.java
@@ -0,0 +1,11 @@
+class DivisibleBy13 {
+    public static void main(String[] args) {
+        int n = 221;
+
+        if (n % 13 == 0)
+            System.out.println("221 is divisible by 13");
+        else
+            System.out.println("221 is not divisible by 13");
+    }
+}
+ 
---
+++ positive and negative.java
@@ -0,0 +1,11 @@
+class PositiveNegative {
+ public static void main(String[] args) {
+ int num = -10;                                                                                                                                                 
+if (num > 0)
+  System.out.println("Positive number");
+ else if (num < 0)
+   System.out.println("Negative number");
+ else
+ System.out.println("Zero");
+    }
+}
