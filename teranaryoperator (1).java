8650c6e9c30c3f83e4c13dabb5ef33968b8fb986
unvithakl-hue <unvithakl@gmail.com>
2026-09-02 10:24:17 +0530

    Add files via upload

---
+++ teranaryoperator (1).java
@@ -0,0 +1,8 @@
+public class teranaryoperator {
+    public static void main(String args[]){
+        int a=2;
+        int b=5;
+        int min=(a<b)?a:b;
+        System.out.println(min);
+    }
+}
