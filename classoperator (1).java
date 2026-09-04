e72a3f0570c1478aa9f7fc374c00b0460d812f19
unvithakl-hue <unvithakl@gmail.com>
2026-09-04 18:43:33 +0530

    Add files via upload

---
+++ classoperator (1).java
@@ -0,0 +1,12 @@
+public class classoperator {
+    
+    public static void main(String args[]) {
+
+
+        System.out.println(10<<2); 
+        System.out.println(10<<3);
+        System.out.println(20<<2); 
+        System.out.println(15<<4); 
+
+    }
+}
