861f5432a7c59c27ad4b8fbce90c928af6d532e9
unvithakl-hue <unvithakl@gmail.com>
2026-09-04 18:44:12 +0530

    Add files via upload

---
+++ factorial (1).java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Factorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        long factorial = 1;
+
+        for (int i = 1; i <= n; i++) {
+            factorial = factorial * i;
+        }
+
+        System.out.println("Factorial of " + n + " = " + factorial);
+    }
+}
