817e558bc7c1af31c5f7d86fae656f8f93bd3f83
unvithakl-hue <unvithakl@gmail.com>
2026-09-08 10:39:56 +0530

    Add files via upload

---
+++ class SumOfFourDigit {.java
@@ -0,0 +1,13 @@
+class SumOfFourDigit {
+public static void main(String[] args) {
+ int n = 1234;
+ int sum = 0;
+
+ while (n > 0) {
+  sum = sum + n % 10;
+     n = n / 10;
+        }
+
+ System.out.println("Sum of digits = " + sum);
+    }
+}
